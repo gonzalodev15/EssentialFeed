208869ef24dfd49cede22511615c7161b082486a
Christian Gonzalo Leon Suarez <u201513219@upc.edu.pe>
2025-03-17 19:08:25 -0500

    Move RemoteFeedLoader components to production

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Christian Gonzalo Leon Suarez on 17/03/25.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
